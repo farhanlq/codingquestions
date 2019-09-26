501dce9f8e34df59c433ee17228d7277ea1655d5
Farhan Laeeq <far619@gmail.com>
2019-09-26 13:44:08 +0530

    Updated Code

--- src/com/programs/ds/App.java
+++ src/com/programs/ds/App.java
@@ -3,7 +3,7 @@ package com.programs.ds;
 public class App {
 
 	public static void main(String[] args) {
-		Stack theStack = new Stack(3);
+		//Stack theStack = new Stack(3);
 		/*
 		 * theStack.push(20); theStack.push(40); theStack.push(60); theStack.push(80);
 		 * 
