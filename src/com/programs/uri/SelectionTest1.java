aed848ee677637550c0b92662a6c1289bf5830b7
Farhan Laeeq <far619@gmail.com>
2019-10-03 12:41:17 +0530

    SelectionTest1

---
+++ src/com/programs/uri/SelectionTest1.java
@@ -0,0 +1,21 @@
+package com.programs.uri;
+
+import java.util.Scanner;
+
+public class SelectionTest1 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int A = scn.nextInt();
+		int B = scn.nextInt();
+		int C = scn.nextInt();
+		int D = scn.nextInt();
+		
+		if((B>C && D>A) && ((C+D)>(A+B)) && (C>0 && D>0) && (A%2==0)) {
+			System.out.println("Valores aceitos");
+		}
+		else {
+			System.out.println("Valores nao aceitos");
+		}
+	}
+}
