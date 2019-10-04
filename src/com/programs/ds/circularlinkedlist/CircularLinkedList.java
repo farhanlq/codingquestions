30b709fd27d8252e152efd32ce16125a6b2da300
Farhan Laeeq <far619@gmail.com>
2019-10-04 17:13:13 +0530

    Updated Code

---
+++ src/com/programs/ds/circularlinkedlist/CircularLinkedList.java
@@ -0,0 +1,12 @@
+package com.programs.ds.circularlinkedlist;
+
+public class CircularLinkedList {
+
+	private Node first;
+	private Node last;
+	
+	public CircularLinkedList() {
+		first = null;
+		last = null;
+		}
+}
