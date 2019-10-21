e688a9a67c73105e9ad763d42295d12a01d22584
Farhan Laeeq <far619@gmail.com>
2019-10-21 13:26:26 +0530

    Added .gitignore

--- src/com/programs/ds/stack/BalancedBrackets.java
+++ src/com/programs/ds/stack/BalancedBrackets.java
@@ -2,15 +2,26 @@ package com.programs.ds.stack;
 
 public class BalancedBrackets {
 
-	int top;
-	public static void check() {
-		
+	public static String check(String str) {
+		int stackSize = str.length();
+		Stack stack = new Stack(stackSize);
+		for (int i = 0; i < stackSize; i++) {
+			if (str.charAt(i) == '{' || str.charAt(i) == '(' || str.charAt(i) == '[') {
+				stack.push(str.charAt(i));
+			}
+			if (str.charAt(i) == '}' || str.charAt(i) == ')' || str.charAt(i) == ']') {
+				stack.pop();
+			}
+		}
+		if (stack.isEmpty())
+			return "YES";
+		else
+			return "NO";
 	}
-	
+
 	public static void main(String[] args) {
-		String arr ="{[()]}";
-		char str[] = arr.toCharArray();
-		
-		
+		System.out.println(check("{[()]}"));
+		System.out.println(check("{[(])}"));
+		System.out.println(check("{{[[(())]]}}"));
 	}
 }
