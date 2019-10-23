05e34bebd68c761959efa986ee64b0e555aa2d28
Farhan Laeeq <far619@gmail.com>
2019-10-23 16:34:18 +0530

    Updated Bag Of Numbers

--- src/com/programs/ds/stack/BagOfNumbers.java
+++ src/com/programs/ds/stack/BagOfNumbers.java
@@ -1,22 +1,69 @@
 package com.programs.ds.stack;
 
+class StackBag {
+
+	private int maxSize;
+	private int[] stackArray;
+	private int top;
+
+	public StackBag(int size) {
+		this.maxSize = size;
+		this.stackArray = new int[maxSize];
+		this.top = -1;
+
+	}
+
+	public void push(int i) {
+		if (isFull()) {
+			System.out.println("This stack is already full!!");
+		} else {
+			top++;
+			stackArray[top] = i;
+		}
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			System.out.println("Stack is already empty, You can't pop");
+			return '0';
+		} else {
+			int old_top = top;
+			top--;
+			return stackArray[old_top];
+		}
+	}
+
+	public int peak() {
+		return stackArray[top];
+	}
+
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+
+	public boolean isFull() {
+		return (maxSize - 1 == top);
+	}
+}
+
 public class BagOfNumbers {
 
 	public static void main(String[] args) {
-		Stack theStack = new Stack(4);
+		StackBag theStack = new StackBag(4);
 
-		theStack.push('a');
-		theStack.push('b');
-		theStack.push('d');
-		theStack.push('e');
+		theStack.push(12);
+		theStack.push(34);
+		theStack.push(89);
+		theStack.push(23);
 
-		Stack orgStack = new Stack(4);
+		StackBag orgStack = new StackBag(4);
 		while (!theStack.isEmpty()) {
 			orgStack.push(theStack.pop());
 		}
 
 		while (!orgStack.isEmpty()) {
-			System.out.print("" + orgStack.pop());
+			System.out.print(orgStack.pop()+" ");
+			
 		}
 	}
 }
